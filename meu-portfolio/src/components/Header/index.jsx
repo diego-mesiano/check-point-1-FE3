1362b36ab4d45654787f26fe5e33f4a6f0ffe65d
diego-mesiano <diego.mesiano1@gmail.com>
2021-11-16 19:17:59 -0300

    Ajustes finais de responsividade

--- meu-portfolio/src/components/Header/index.jsx
+++ meu-portfolio/src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import './style.scss';
 function Header() {
 
   return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light" id="cabecalho">
+    <nav className="navbar fixed-top navbar-expand-sm navbar-light bg-light" id="cabecalho">
       <div className="container-fluid">
         <a className="navbar-brand" href="#banner" id="logo">Diego Mesiano</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
